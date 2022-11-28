8f29191d0170716c0301ebd700e7e6ad06af53bc
Erick Olvera <erickfco1999@gmail.com>
2022-11-28 12:58:41 -0600

    Incorporing new routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,26 @@
 import { Routes, Route } from "react-router-dom";
-import InventoryPage from "./pages/InventoryPage";
-import InventoryForm from "./pages/InventoryForm";
+import ProductsPage from "./pages/products/ProductsPage";
+import ProductsForm from "./pages/products/ProductsForm";
+import ProvidersPage from "./pages/providers/ProvidersPage";
 import PageNotFound from "./pages/PageNotFound";
+import ServerErrorPage from "./pages/ServerErrorPage";
 import Navbar from "./components/Navbar";
 import "./App.css";
-import ProvidersPage from "./pages/ProvidersPage";
+import FormUpdate from "./pages/providers/FormUpdate";
+import TakeItemsAwayPage from "./pages/products/TakeItemsAwayPage";
 
 function App() {
   return (
     <div className="bg-neutral-800 h-screen">
       <Navbar />
       <Routes>
-        <Route path="/" element={<InventoryPage />} />
-        <Route path="/newStock" element={<InventoryForm />} />
+        <Route path="/" element={<ProductsPage />} />
+        <Route path="/newStock" element={<ProductsForm />} />
+        <Route path="/deleteProducts" element={<TakeItemsAwayPage />} />
         <Route path="/providers" element={<ProvidersPage />} />
+        <Route path="/providers/:id" element={<FormUpdate />} />
         <Route path="*" element={<PageNotFound />} />
+        <Route path="/error" element={<ServerErrorPage />} />
       </Routes>
     </div>
   );
