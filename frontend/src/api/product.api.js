89014f9833dcb269e8af8780d9832d68d3fa902f
Erick Olvera <erickfco1999@gmail.com>
2022-11-28 12:57:25 -0600

    New API routes

--- frontend/src/api/product.api.js
+++ frontend/src/api/product.api.js
@@ -2,5 +2,20 @@ import axios from "axios"
 export const getProducts = async() => 
     await axios.get("http://localhost:5000/products")
 
+export const getStock = async(id) => 
+    await axios.get(`http://localhost:5000/stock/${id}`)
+
 export const createProduct = async(product) => 
     await axios.post("http://localhost:5000/products", product)
+
+export const updateProduct = async(id, product) => 
+    await axios.put(`http://localhost:5000/products/${id}`, product)
+
+export const deleteStockProduct = async(id, product) => 
+    await axios.put(`http://localhost:5000/stock_delete/${id}`, product)
+
+export const addStockProduct = async(id, product) => 
+    await axios.put(`http://localhost:5000/stock_add/${id}`, product)
+
+export const deleteProduct = async(id) => 
+    await axios.delete(`http://localhost:5000/products/${id}`)
--- frontend/src/api/provider.api.js
+++ frontend/src/api/provider.api.js
@@ -1,4 +1,15 @@
 import axios from "axios";
 export const getProviders = async () => await axios.get("http://localhost:5000/providers")
 
+export const getProvider = async (id) => await axios.get(`http://localhost:5000/providers/${id}`)
+
 export const createProvider = async (provider) => await axios.post("http://localhost:5000/providers", provider)
+
+export const updateProvider = async (id, provider) => {
+    return await axios.put(`http://localhost:5000/providers/${id}`, {
+        name_prov: provider.name_prov,
+        email: provider.email,
+        phone: provider.phone,
+    })
+}
+export const deleteProvider = async (id) => await axios.delete(`http://localhost:5000/providers/${id}`)
