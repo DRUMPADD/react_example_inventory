e51d099be09a931a2518a731646d13f28357eca8
Erick Olvera <erickfco1999@gmail.com>
2022-11-28 12:54:18 -0600

    New routes

--- backend/routes/products.routes.js
+++ backend/routes/products.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import {getProducts, getProduct, createProduct, updateProduct, deleteProduct} from '../controllers/product.controller.js';
+import {getProducts, getProduct, createProduct, updateProduct, deleteProduct, deleteStock, getProductStock, addStock} from '../controllers/product.controller.js';
 const router = Router();
 
 router.get("/products", getProducts);
 router.get("/products/:id", getProduct);
+router.get("/stock/:id", getProductStock);
 router.post("/products", createProduct);
+router.put("/stock_delete/:id", deleteStock);
+router.put("/stock_add/:id", addStock);
 router.put("/products/:id", updateProduct);
 router.delete("/products/:id", deleteProduct);
 
