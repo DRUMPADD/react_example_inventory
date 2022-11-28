cf887243b826e0c8b8bad9dde9badcc43fdd3ada
Erick Olvera <erickfco1999@gmail.com>
2022-11-28 12:59:02 -0600

    Server error page

---
+++ frontend/src/pages/ServerErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ServerErrorPage() {
+  return (
+    <div className='text-center font-bold text-white'>
+        <h1>Oh, no! Something happened</h1>
+        <p>Please try in another moment</p>
+    </div>
+  )
+}
+
+export default ServerErrorPage
