81a299899457d9bf56621f3bd86d58a4ed33491a
Erick Olvera <erickfco1999@gmail.com>
2022-11-28 12:57:02 -0600

    Adding link to update

--- frontend/src/components/ProvidersCard.jsx
+++ frontend/src/components/ProvidersCard.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function ProvidersCard({ provider }) {
   return (
-    <div className='bg-neutral-700 rounded-md text-white p-2 my-4 w-4/5'>
-      <p className='text-2xl font-bold my-2 text-center'>{provider.prov} - {provider.name_prov}</p>
-      <p className='text-xl my-2 text-center'>Email: {provider.email}</p>
-      <p className='text-xl my-2 text-center'>Phone: {provider.phone}</p>
-    </div>
+    <Link to={`/providers/${provider.prov}`}>
+      <div className='bg-neutral-700 rounded-md text-white p-2 my-4 w-4/5'>
+        <p className='text-2xl font-bold my-2 text-center'>{provider.prov} - {provider.name_prov}</p>
+        <p className='text-xl my-2 text-center'>Email: {provider.email}</p>
+        <p className='text-xl my-2 text-center'>Phone: {provider.phone}</p>
+      </div>
+    </Link>
   )
 }
 
